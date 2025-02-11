6f644b5db9f821699428d587f6eec7656297a53e
Victoria Guazzelli <166502413+VLGuazzelli@users.noreply.github.com>
2025-02-11 23:30:22 +0000

    cambiando app.py

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ label_encoder_city_of_birth=load(open("./src/le_city_of_birth.sav", "rb"))
 label_encoder_country_of_birth=load(open("./src/le_country_of_birth.sav", "rb"))
 label_encoder_competition_id=load(open("./src/le_competition_id.sav", "rb"))
 label_encoder_club_name=load(open("./src/le_club_name.sav", "rb"))
-label_encoder_foot=load(open("./src/le_foot", "rb"))
+label_encoder_foot=load(open("./src/le_foot.sav", "rb"))
 label_encoder_sub_position=load(open("./src/le_sub_position.sav", "rb"))
 label_encoder_position=load(open("./src/le_position.sav", "rb"))
 
@@ -71,35 +71,15 @@ city_of_birth= st.selectbox('Select a city of birth', data['city_of_birth'])
 competition_id = st.selectbox('Select a competition', data['competition_id'])
 
 
-
-
-competition_id_le = label_encoder_competition_id.transform(competition_id)
-club_name_le = label_encoder_club_name.transform(club_name)
-foot_le = label_encoder_foot.transform(foot)
-position_le = label_encoder_position.transform(position)
-sub_position_le	 = label_encoder_sub_position.transform(sub_position)
-country_of_birth_le = label_encoder_country_of_birth.transform(country_of_birth)
-city_of_birth_le = label_encoder_city_of_birth.transform(city_of_birth)
+competition_id_le = label_encoder_competition_id.transform([competition_id])
+club_name_le = label_encoder_club_name.transform([club_name])
+foot_le = label_encoder_foot.transform([foot])
+position_le = label_encoder_position.transform([position])
+sub_position_le	 = label_encoder_sub_position.transform([sub_position])
+country_of_birth_le = label_encoder_country_of_birth.transform([country_of_birth])
+city_of_birth_le = label_encoder_city_of_birth.transform([city_of_birth])
 
 info=[matches_played, yellow_cards, red_cards, goals, assists, minutes_played, age, height_in_cm, highest_market_value_in_eur, competition_id_le, club_name_le, foot_le, position_le, sub_position_le, country_of_birth_le,  city_of_birth_le]
-
-# Contenido principal
-st.write(f'La matches_played seleccionada es {matches_played}')
-st.write(f'La yellow_cards seleccionada es {yellow_cards}')
-st.write(f'La red_cards seleccionada es {red_cards}')
-st.write(f'La goals seleccionada es {goals}')
-st.write(f'La assists seleccionada es {assists}')
-st.write(f'La minutes_played seleccionada es {minutes_played}')
-st.write(f'La age seleccionada es {age}')
-st.write(f'La height_in_cm seleccionada es {height_in_cm}')
-st.write(f'La highest_market_value_in_eur seleccionada es {highest_market_value_in_eur}')
-st.write(f'La competition_id_le seleccionada es {competition_id_le}')
-st.write(f'La club_name_le seleccionada es {club_name_le}')
-st.write(f'La foot_le seleccionada es {foot_le}')
-st.write(f'La position_le seleccionada es {position_le}')
-st.write(f'La sub_position_le seleccionada es {sub_position_le}')
-st.write(f'La country_of_birth_le seleccionada es {country_of_birth_le}')
-st.write(f'La city_of_birth_le seleccionada es {city_of_birth_le}')
 
 
 if st.button("Realizar predicción"):
